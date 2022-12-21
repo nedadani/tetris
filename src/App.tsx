d8cdf76b410b6a35f9cc23f211e696439e124bf7
nedakinduryte <neda.kinduryte@gmail.com>
2022-12-21 17:42:50 +0000

    removed controls logic from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
+import { useAtom } from 'jotai';
+import { useAtomValue } from 'jotai/utils';
 
 import Board from './components/Board';
+import { useControls, usePlayer } from './utils/hooks';
+import { gameOverAtom } from './atoms';
+
+import styles from './App.module.css';
 
 const App = () => {
-  return <Board />;
+  return (
+    <div className={styles.wrapper}>
+      <Board />
+    </div>
+  );
 };
 
 export default App;
