167adced04ba3968f878b9165e0d9448227f56d4
nedakinduryte <neda.kinduryte@gmail.com>
2023-02-07 22:31:17 -0500

    updated types

--- src/constants/defaultCell.ts
+++ src/constants/defaultCell.ts
@@ -1,3 +1,6 @@
-const DEFAULT_CELL = { isEmpty: true, value: 0 };
+type DefaultCellType = { isEmpty: boolean; value: number };
+
+const DEFAULT_CELL: DefaultCellType = { isEmpty: true, value: 0 };
 
 export { DEFAULT_CELL };
+export type { DefaultCellType };
--- src/utils/createBoard.ts
+++ src/utils/createBoard.ts
@@ -1,12 +1,12 @@
-import { BOARD_HEIGHT, BOARD_WIDTH, DEFAULT_CELL } from '../constants';
+import { BOARD_HEIGHT, BOARD_WIDTH, DEFAULT_CELL, DefaultCellType } from '../constants';
 
-const createBoard = () => {
+type CreateBoardTypes = DefaultCellType[][];
+
+const createBoard = (): CreateBoardTypes => {
   return Array.from({ length: BOARD_HEIGHT }, () =>
     Array.from({ length: BOARD_WIDTH }, () => ({ ...DEFAULT_CELL }))
   );
 };
-
-type CreateBoardTypes = ReturnType<typeof createBoard>;
 
 export { createBoard };
 export type { CreateBoardTypes };
