d2b372982ca93053507a197ef80477a834174f4e
nedakinduryte <neda.kinduryte@gmail.com>
2023-02-13 11:28:44 -0500

    added clear rows functionality

--- src/utils/hooks/useBoard.ts
+++ src/utils/hooks/useBoard.ts
@@ -1,18 +1,31 @@
 import { useState, useEffect } from 'react';
 
 import { createBoard, CreateBoardTypes } from '../../utils';
-import { DEFAULT_CELL } from '../../constants';
+import { DEFAULT_CELL, DefaultCellType } from '../../constants';
 import { PlayerTypes } from '../../utils/hooks';
 
 const useBoard = (player: PlayerTypes, resetPlayer: () => void) => {
   const [board, setBoard] = useState<CreateBoardTypes>(createBoard());
 
   useEffect(() => {
+    const clearRows = (newBoard: CreateBoardTypes) =>
+      newBoard.reduce((acc, row) => {
+        // if no empty cells in the row
+        if (row.findIndex((cell) => cell.value === 0) === -1) {
+          const emptyRow = new Array<DefaultCellType>(newBoard[0].length).fill(DEFAULT_CELL);
+          acc.unshift(emptyRow);
+        } else {
+          acc.push(row);
+        }
+        return acc;
+      }, [] as DefaultCellType[][]);
+
     const updateBoard = (prevBoard: CreateBoardTypes) => {
       const newBoard = prevBoard.map((row) =>
         row.map((cell) => (cell.isEmpty ? { ...DEFAULT_CELL } : cell))
       );
 
+      // TODO: find bug here
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value) {
@@ -23,13 +36,14 @@ const useBoard = (player: PlayerTypes, resetPlayer: () => void) => {
 
       if (player.collided) {
         resetPlayer();
+        return clearRows(newBoard);
       }
 
       return newBoard;
     };
 
     setBoard((prev) => updateBoard(prev));
-  }, [player, resetPlayer]);
+  }, [player, resetPlayer, setBoard]);
 
   return { board, setBoard };
 };
