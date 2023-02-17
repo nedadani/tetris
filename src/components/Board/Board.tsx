eae10ab72930f4bab17f4f70b282fde410a9b434
nedakinduryte <neda.kinduryte@gmail.com>
2023-02-17 12:24:54 -0500

    added auto focus on game area

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { useAtom, useAtomValue } from 'jotai';
+import { FC, useEffect, useRef } from 'react';
+import { useAtomValue } from 'jotai';
 
 import { gameOverAtom } from '../../atoms';
 import { usePlayer, useBoard, useControls } from '../../utils/hooks';
@@ -8,10 +8,15 @@ import styles from './Board.module.css';
 
 const Board: FC = () => {
   const isGameOver = useAtomValue(gameOverAtom);
+  const gameArea = useRef<HTMLDivElement>(null);
 
   const { player, resetPlayer, updatePlayerPos, rotatePlayer } = usePlayer();
   const { board, setBoard } = useBoard(player, resetPlayer);
   const { handleKeydown } = useControls(rotatePlayer, updatePlayerPos, player, board);
+
+  useEffect(() => {
+    if (gameArea.current) gameArea.current.focus();
+  }, []);
 
   return (
     <div
@@ -19,6 +24,7 @@ const Board: FC = () => {
       role="button"
       tabIndex={0}
       onKeyDown={(e) => handleKeydown(e.key)}
+      ref={gameArea}
     >
       {board.map((row, xIdx) =>
         row.map((cell, yIdx) => <div key={`${xIdx}-${yIdx}`}>{cell.value}</div>)
