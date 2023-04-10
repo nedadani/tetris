138a2d7ff78eafe192d701de19bbfadf7b0d98d1
nedakinduryte <neda.kinduryte@gmail.com>
2023-04-10 16:43:14 -0400

    added the start game modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useEffect, useRef } from 'react';
+import { useAtomValue } from 'jotai';
+
 import Board from './components/Board';
+import StartGameModal from './components/StartGameModal';
+
+import { startGameAtom } from './atoms';
 
 import styles from './App.module.css';
 
 const App = () => {
+  const showStartGameModal = useAtomValue(startGameAtom);
   const gameArea = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (gameArea.current?.firstChild) {
       (gameArea.current.firstChild as HTMLButtonElement).focus();
     }
-  }, [gameArea.current]);
+  }, [showStartGameModal]);
+
+  if (showStartGameModal) {
+    return (
+      <div className={styles.wrapper}>
+        <StartGameModal />
+        <Board />
+      </div>
+    );
+  }
 
   return (
     <div className={styles.wrapper} ref={gameArea}>
